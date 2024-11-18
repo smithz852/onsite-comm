70f0e19a9433e85ea0647c5cbe17cbf3779ae457
Zach Smith <zachsmith852@gmail.com>
2024-11-17 20:32:27 -0800

    updating wireframe files

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,19 @@
+import './Header.css'
+
 export default function Header() {
   return (
     <>
-      <h1>Onsite Communications</h1>
+    <div className='headerFlex'>
+      <div className="headerItemFlex">
+         <h4>Onsite Communications</h4>
+         <button>Discover</button>
+      </div>
+      <div className="headerItemFlex">
+        <input type="text" id='searchBar'/>
+         <button>Profile</button>
+         <button id='loginBtnMargin'>Login</button>
+      </div>
+      </div>
     </>
   )
 }
