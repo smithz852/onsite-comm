b2cdc06cf62d5cbcfa11c19d95fdf79368a53be0
Zach Smith <zachsmith852@gmail.com>
2024-11-19 21:19:06 -0800

    seed db

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,14 @@
 // import { Query } from 'mongoose'
 import Categories from '../models/index.js'
+import SubCategory from '../models/SubCategory.js';
 
 const resolvers = {
   Query: {
     categories: async () => {
       return Categories.find()
+    },
+    subCategory: async ({ category, name }) => {
+      return SubCategory.find()
     }
   }
 }
