8abd831fe0126f692d932a9448e8a7d30e2b0f55
Zach Smith <zachsmith852@gmail.com>
2024-11-20 18:41:23 -0800

    complete seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,10 +1,14 @@
 import db from '../config/connection.js'
 import Categories from '../models/index.js'
+import SubCategory from '../models/SubCategory.js';
 import categorySeeds from './categorySeeds.json' assert { type: 'json' };
+import subCategories from './subCategories.json' assert { type: 'json' };
 
 db.once('open', async () => {
   await Categories.insertMany(categorySeeds);
+  await SubCategory.insertMany(subCategories)
   console.log(categorySeeds)
+  console.log(subCategories)
   // console.log(db)
   console.log('completed seeding')
   process.exit(0);
