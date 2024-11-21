6cf770c7694b2aa6479ee2831d8de55dd45465c9
Zach Smith <zachsmith852@gmail.com>
2024-11-20 21:22:28 -0800

    get server file functioning properly

--- server/config/connection.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/OnsiteComm')
-
-const connection = mongoose.connection
-export default connection;
